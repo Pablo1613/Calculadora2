f784c431ed197b9f3f2aaf23cf4811911427c1d9
Pablo1613 <4077812@alu.murciaeduca.es>
2026-01-05 00:17:03 +0100

    Update Calculadora.java

--- Calculadora.java
+++ Calculadora.java
@@ -2,5 +2,6 @@ public class Calculadora {
     public static void main(String[] args) {
         System.out.println("Calculadora2");
         //Modificación por comandos
+        //Modificación desde github
     }
 }
